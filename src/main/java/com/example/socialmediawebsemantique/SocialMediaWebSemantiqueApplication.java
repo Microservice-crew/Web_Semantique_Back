54f1a01d186bfba8c131d29d75531a85a7be2393
Zied.mathlouthi@esprit.tn <zied.mathlouthi@esprit.tn>
2023-10-28 19:37:07 +0100

    First Commit

---
+++ src/main/java/com/example/socialmediawebsemantique/SocialMediaWebSemantiqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialmediawebsemantique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaWebSemantiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaWebSemantiqueApplication.class, args);
+    }
+
+}
